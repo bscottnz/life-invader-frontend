bcba9a0ac3f3e16c2a350e29386200f8abfb6933
Ben <blscott@gmail.com>
2021-05-20 14:19:43 +1200

    basic profile page setup

--- src/components/MainPage/MainContent/ProfilePage.js
+++ src/components/MainPage/MainContent/ProfilePage.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
+
+import { HiOutlineEmojiSad } from 'react-icons/hi';
 
 const ProfilePage = () => {
+  // const [profileUsername, setProfileUsername] = useState('');
+
+  const profileName = useParams().username;
+
+  // show error message if no user is returned
+  const [showError, setShowError] = useState(false);
+
+  const getProfileData = () => {
+    axios({
+      method: 'get',
+      withCredentials: true,
+      url: `${process.env.REACT_APP_BASE_URL}/api/profile/${profileName}`,
+    })
+      .then((res) => {
+        setShowError(false);
+        console.log(res.data);
+      })
+      .catch((err) => {
+        setShowError(true);
+      });
+  };
+
+  useEffect(() => {
+    getProfileData();
+  }, [profileName]);
+
   return (
     <div>
-      <h1>Profile page yo</h1>
+      <h1 className="main-content-heading">{profileName}</h1>
+      {showError && (
+        <div>
+          <h2>User not found</h2>
+          <HiOutlineEmojiSad style={{ fontSize: '50px', marginTop: '15px' }} />
+        </div>
+      )}
     </div>
   );
 };
