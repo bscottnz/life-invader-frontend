5c6b17a751582e9fe647312fdbdfc7dc05345fad
Ben <blscott@gmail.com>
2021-06-02 17:42:04 +1200

    loading page spinner

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,9 +1,13 @@
 import React from 'react';
 
+import { VscLoading } from 'react-icons/vsc';
+
 const Loading = () => {
   return (
-    <div>
-      <h1>Loading...</h1>
+    <div
+      style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}
+    >
+      <VscLoading className="spinner" style={{ fontSize: '40px', color: '#1da1f2' }} />
     </div>
   );
 };
